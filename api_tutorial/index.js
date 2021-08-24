46f7794519b8117dc64d823146578c9fb5cd8b7e
nome <email>
2021-08-24 11:51:24 -0300

    data 24/08 . . . . . .baixamos o INSOMNIA

---
+++ api_tutorial/index.js
@@ -0,0 +1,58 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+// Rotas da raiz
+app.get("/", (request, response) => {
+    console.log(request);
+    console.log(response);
+    response.send("Hello world!");
+});
+
+app.post("/", (request, response) => {
+    response.send("Método POST");
+});
+
+app.put("/", (request, response) => {
+    response.send("Método PUT");
+});
+
+app.delete("/", (request, response) => {
+    response.send("Método DELETE");
+});
+//Banco de dados
+const users = [
+    { id: 1, name: "Pedro", email: "pedro@email.com" },
+    { id: 2, name: "João", email: "joao@email.com" },
+    { id: 3, name: "Marcos", email: "marcos@email.com" },
+];
+
+// Rotas do usuario "/users"
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+
+    const user = users.find(user => user.id == userId);
+
+    if (!user) {
+        res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+});
+
+app.get("/users", (req, res) => {
+
+});
+
+app.get("/users", (req, res) => {
+
+});
+
+app.get("/users", (req, res) => {
+
+});
+
+app.listen(PORT, () => console.log("O servidor está rodando..."));
