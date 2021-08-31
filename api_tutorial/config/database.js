fc5e4a23d1b2f19a17fa1dea45446385a5fdbb5f
nome <email>
2021-08-31 08:31:06 -0300

    comecei a aula terça

--- api_tutorial/config/database.js
+++ api_tutorial/config/database.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 module.exports = {
     development: {
-        username: process.env.PG_USERNAME = postgres,
-        password: process.env.PG_PASSWORD = 123456,
-        database:  process.env.PG_DATABASE = tutorial_bd,
-        host:  process.env.PG_HOST = localhost,
-        port:  process.env.PG_PORT = 5432,
-        dialect:  process.env.BD_DIALECT = postgres,
+        username: process.env.PG_USERNAME,
+        password: process.env.PG_PASSWORD,
+        database:  process.env.PG_DATABASE,
+        host:  process.env.PG_HOST,
+        port:  process.env.PG_PORT,
+        dialect:  process.env.BD_DIALECT,
         logging: false,
         define: {
             underscored: true
--- api_tutorial/db/models/user.js
+++ api_tutorial/db/models/user.js
@@ -2,21 +2,39 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require("bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate(models) { }
+    isPasswordValid(password) {
+      return bcrypt.compareSync(password, this.password);
     }
   };
   User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: { msg: "Invalid E-mail!" }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(password) {
+        this.setDataValue("password", bcrypt.hashSync(password, 10));
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
